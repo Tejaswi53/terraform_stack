ce6df491cfd05d0617c351a241c311bf88f24ddf
rahulinti65 <intirahul4@gmail.com>
2025-06-30 01:58:24 -0500

    Update vpc.tf

--- stacks/kasier/vpc.tf
+++ stacks/kasier/vpc.tf
@@ -2,7 +2,7 @@ module "vpc" {
   source               = "../Modules"
   cidr                 = var.cidr_block
   Name                 = var.Name
-  create_vpc           = var.create_vpc
+  create_vpc           = true
   enable_dns_hostnames = var.enable_dns_hostnames
   enable_dns_support   = var.enable_dns_support
   public_subnets_cidr  = var.public_subnets_cidr
