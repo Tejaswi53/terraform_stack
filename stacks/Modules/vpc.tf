0aa0d98c682d343fd886d4130b7f99704c378e04
Tejaswi53 <mtejaswi53@gmail.com>
2025-07-04 16:30:12 +0530

    private

--- stacks/Modules/vpc.tf
+++ stacks/Modules/vpc.tf
@@ -41,7 +41,7 @@ resource "aws_subnet" "private-subnet" {
 }
 
 resource "aws_nat_gateway" "ngw" {
-  subnet_id = aws_subnet.public-subnet[0].id
+  subnet_id = aws_subnet.public-subnet-[0].id
 }
 
 resource "aws_route_table" "private-rt" {
