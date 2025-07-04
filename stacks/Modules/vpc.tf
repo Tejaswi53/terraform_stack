3a3b3b58134fde936bf602f145f07cef89e3be56
Tejaswi53 <mtejaswi53@gmail.com>
2025-07-04 17:20:17 +0530

    ngwremoved

--- stacks/Modules/vpc.tf
+++ stacks/Modules/vpc.tf
@@ -46,12 +46,12 @@ resource "aws_nat_gateway" "ngw" {
 }
 
 resource "aws_route_table" "private-rt" {
-  vpc_id   = aws_vpc.vpc[0].id
-  for_each = aws_nat_gateway.ngw
+  vpc_id = aws_vpc.vpc[0].id
+
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_nat_gateway.ngw[each.key].id
+
   }
 }
 
